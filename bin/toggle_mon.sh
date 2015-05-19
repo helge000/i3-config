43ea6d723820b421f279658be8ae3d0040f43022
Daniel Helgenberger <daniel.helgenberger@m-box.de>
2015-05-19 11:08:01 +0200

    New: Switch Mon Res via Mod+Shift+Tab DANIEL HELGENBERGER <daniel.helgenberger@m-box.de>

---
+++ bin/toggle_mon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IN="LVDS1"
+EXT="HDMI3"
+
+if (xrandr | grep "$EXT disconnected"); then
+      xrandr --output $EXT --off --output $IN --auto --primary
+      i3 restart
+      nitrogen --restore
+else
+      xrandr --output $IN --off --output $EXT --auto --primary
+      i3 restart
+      nitrogen --restore
+fi
+
+# Use hotkeys for acpid switch
+# ibm/hotkey LEN0068:00 00000080 00004011
+# ibm/hotkey LEN0068:00 00000080 00004010
+
