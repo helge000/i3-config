6cf399919e5dcedb037f9e529705a3542993c8a7
Daniel Helgenberger <daniel.helgenberger@m-box.de>
2016-10-04 17:45:16 +0200

    Switch to comton config file

--- bin/toggle_mon.sh
+++ bin/toggle_mon.sh
@@ -5,14 +5,10 @@ EXT="DP-2-2"
 
 if (xrandr | grep "$EXT connected"); then
   xrandr --output $IN --off --output ${EXT} --auto --primary
-  #killall compton
-  #compton -CGb --backend glx --vsync opengl --xrender-sync
   i3 restart
   nitrogen --restore
 else
   xrandr --output ${EXT} --off --output $IN --auto --primary
-  #killall compton
-  #compton -CGb --backend glx --vsync opengl --xrender-sync
   i3 restart
   nitrogen --restore
 fi
