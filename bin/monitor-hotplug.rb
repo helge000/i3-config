e9452c62cde18168acda62bc4ebebe09b240b89a
Daniel Helgenberger <daniel.helgenberger@m-box.de>
2017-05-03 17:30:55 +0200

    Fix: X server unresponsive due to xrandr call
    xrandr was called every SLEEPSECS to check for new connected inputs.
    However, this method proved to be quite costly as it introduced a
    'hang' in X server on every loop.
    
    Now, edid's are checked and validated against stored values to detect
    display changes.

--- bin/monitor-hotplug.rb
+++ bin/monitor-hotplug.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+
+require 'digest'
+
 IN = 'eDP-1'
 SLEEPSECS = 5
 PIDFILE = File.join ENV['HOME'], '.monitor-hotplug.pid'
@@ -7,6 +10,10 @@ PRECMD = ['xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 
 POSTCMD = ['nitrogen --restore']
 IN_APPEND = ' --mode 1920x1080'
 EXT_APPEND = ' --auto --primary'
+DRICARD = 'card0'
+
+@dripath = File.join('/sys', %x(udevadm info -q path -n /dev/dri/#{DRICARD}).chomp)
+puts "Using sys path: #{@dripath}"
 
 def shutdown
   File.delete(PIDFILE) if File.exist? PIDFILE
@@ -22,6 +29,26 @@ end
 
 def execute(cmd)
   DEBUG ? (puts cmd) : (%x(#{cmd}))
+end
+
+def edidchanged?
+  @edid ||= []
+  curr_edid = []
+  puts "checking #{@dripath}/#{DRICARD}-*" if DEBUG
+  Dir.glob("#{@dripath}/#{DRICARD}-*").each do |dir|
+    puts "checking edid path #{dir}" if DEBUG
+    edid_f = File.join(dir, 'edid')
+    next unless File.exist?(edid_f)
+    curr_edid << Digest::MD5.file(edid_f).to_s
+  end
+  puts "Stored EDIDs: #{@edid}\nCurrent EDIDs: #{curr_edid}" if DEBUG
+  unless @edid.sort == curr_edid.sort
+    @edid = curr_edid
+    puts 'EDIDs changed' if DEBUG
+    return true
+  end
+  puts 'EDIDs unchanged' if DEBUG
+  false
 end
 
 PRECMD.each do |cmd|
@@ -40,24 +67,21 @@ end
 
   loop do
     puts 'Running loop' if DEBUG
-    @displays ||= []
+    sleep SLEEPSECS
+    next unless edidchanged?
     _displays = []
     %x(xrandr).split("\n").each { |l| _displays << l.gsub(/ .*/, '') if l =~ / connected/}
-    unless @displays.sort == _displays.sort
-      @displays = _displays
-      external = (_displays - [IN]).first
-      if external # external monitor
-        @external = external
-        xrandr = "xrandr --output #{IN} --off --output #{@external}#{EXT_APPEND}"
-      else
-        xrandr = "xrandr --output #{@external||= 'UDEF'} --off --output #{IN} --primary#{IN_APPEND}"
-      end
-      execute xrandr
-      POSTCMD.each do |cmd|
-        execute cmd
-      end
+    external = (_displays - [IN]).first
+    if external # external monitor
+      @external = external
+      xrandr = "xrandr --output #{IN} --off --output #{@external}#{EXT_APPEND}"
+    else
+      xrandr = "xrandr --output #{@external||= 'UDEF'} --off --output #{IN} --primary#{IN_APPEND}"
     end
-    sleep SLEEPSECS
+    execute xrandr
+    POSTCMD.each do |cmd|
+      execute cmd
+    end
   end
 end
 
